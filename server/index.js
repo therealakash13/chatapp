76cabb8de085ebf152630a6dbce93363dd7dd662
Akash <therealakash13@gmail.com>
2024-05-16 21:17:43 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const userRoutes = require("./routes/userRoutes");
+const messageRoutes = require("./routes/messageRoutes");
+const socket = require("socket.io");
+const app = express();
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", userRoutes);
+app.use("/api/messages", messageRoutes);
+
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log("Connection to Database Successfull");
+}).catch((err) => {
+    console.log(err.message);
+});
+
+const server = app.listen(process.env.PORT,() => {
+    console.log(`Server Started Successfully at Port : ${process.env.PORT}`);
+});
+
+const io = socket( server, {
+    cors: {
+        origin: "http://localhost:3000",
+        credentials: true,
+    },
+});
+
+global.onlineUsers = new Map();
+
+io.on("send-msg", (data) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    if(sendUserSocket) {
+        socket.to(sendUserSocket).emit("msg-recieve", data.message);
+    }
+});
